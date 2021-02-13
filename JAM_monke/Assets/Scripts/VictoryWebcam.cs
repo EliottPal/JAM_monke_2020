57c4c95ccdd8126a0e734ff2536fc82868557413
Eliott Pal <eliott.^palueau@epitech.eu>
2021-02-13 19:16:17 +0100

    Adding victory scene + webcam script a tester

---
+++ JAM_monke/Assets/Scripts/VictoryWebcam.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryWebcam : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        WebCamTexture myWebcam = new WebCamTexture();
+        Debug.Log(myWebcam.deviceName);
+
+        Renderer camRenderer = GetComponent<Renderer>();
+        camRenderer.material.mainTexture = myWebcam;
+        myWebcam.Play();
+    }
+}
