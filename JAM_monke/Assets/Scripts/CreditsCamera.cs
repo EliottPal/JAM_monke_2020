e086748be830ac3e26f8ffc0de43ba60e6df7783
Eliott Pal <eliott.^palueau@epitech.eu>
2021-02-13 15:53:45 +0100

    Adding credits scene avec défilement

---
+++ JAM_monke/Assets/Scripts/CreditsCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsCamera : MonoBehaviour
+{
+    public float forwardSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0f, forwardSpeed * Time.deltaTime, 0f, null);
+        Debug.Log(transform.position.y);
+        if (transform.position.y >= 3810)
+            forwardSpeed = 0;
+    }
+}
