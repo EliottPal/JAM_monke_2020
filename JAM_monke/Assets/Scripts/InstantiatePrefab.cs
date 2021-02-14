3c8d85fbdd0cedef19975c8d0daa756921a9eb9b
Assurio <meven.dugor@epitech.eu>
2021-02-14 21:37:08 +0100

    update position spawn car

--- JAM_monke/Assets/Scripts/InstantiatePrefab.cs
+++ JAM_monke/Assets/Scripts/InstantiatePrefab.cs
@@ -21,43 +21,43 @@ public class InstantiatePrefab : MonoBehaviour
         if (Manager.Instance.Value == "Groot")
         {
             var loadedPrefabResource = LoadPrefabFromFile("Past/Medieval village/Cart");
-            Instantiate(loadedPrefabResource, new Vector3((float)-255, (float)-8, (float)117), Quaternion.identity);
+            Instantiate(loadedPrefabResource, new Vector3((float)-255, (float)-8, (float)117), transform.rotation * Quaternion.Euler (0f, 39.28f, 0f));
         }
 
         if (Manager.Instance.Value == "Mercold")
         {
             var loadedPrefabResource = LoadPrefabFromFile("Past/MercedesOld/source/Mercold");
-            Instantiate(loadedPrefabResource, new Vector3((float)-260, (float)-7.8, (float)100), Quaternion.identity);
+            Instantiate(loadedPrefabResource, new Vector3((float)-254.5, (float)-7.5, (float)118),transform.rotation * Quaternion.Euler (0f, 39.6f, 0f));
         }
 
         if (Manager.Instance.Value == "Pierrafeu")
         {
             var loadedPrefabResource = LoadPrefabFromFile("Past/Pierrafeu/Pierrafeu");
-            Instantiate(loadedPrefabResource, new Vector3((float)-260, (float)-7.8, (float)100), Quaternion.identity);
+            Instantiate(loadedPrefabResource, new Vector3((float)-253.5, (float)-8.5, (float)118.5), transform.rotation * Quaternion.Euler (0f, 40.23f, 0f));
         }
 
         if (Manager.Instance.Value == "FefeLambo")
         {
             var loadedPrefabResource = LoadPrefabFromFile("Present/Azerilo/HQ Racing Car Model No.1203/Prefabs/Féfé Lambo");
-            Instantiate(loadedPrefabResource, new Vector3((float)-254, (float)-8.5, (float)118), Quaternion.identity);
+            Instantiate(loadedPrefabResource, new Vector3((float)-254, (float)-8.5, (float)118), transform.rotation * Quaternion.Euler (0f, 37.69f, 0f));
         }
 
         if (Manager.Instance.Value == "458Italia")
         {
             var loadedPrefabResource = LoadPrefabFromFile("Present/Ferrari Italia/source/458 Italia");
-            Instantiate(loadedPrefabResource, new Vector3((float)-255, (float)-8.8, (float)116), Quaternion.identity);
+            Instantiate(loadedPrefabResource, new Vector3((float)-255, (float)-8.8, (float)116), transform.rotation * Quaternion.Euler (0f, 36.2f, 0f));
         }
 
         if (Manager.Instance.Value == "TerTer")
         {
             var loadedPrefabResource = LoadPrefabFromFile("Present/Best Sports CARS - Pro 3D Models/Vehicle/SportCar20/Prefabs/Ter Ter");
-            Instantiate(loadedPrefabResource, new Vector3((float)-255, (float)-8.5, (float)117.5), Quaternion.identity);
+            Instantiate(loadedPrefabResource, new Vector3((float)-255, (float)-8.5, (float)117.5), transform.rotation * Quaternion.Euler (0f, 36.02f, 0f));
         }
 
         if (Manager.Instance.Value == "Cyberpunk")
         {
             var loadedPrefabResource = LoadPrefabFromFile("Future/CyberpunkHovercar/source/CyberpunkHovercar");
-            Instantiate(loadedPrefabResource, new Vector3((float)-260, (float)-7.8, (float)100), Quaternion.identity);
+            Instantiate(loadedPrefabResource, new Vector3((float)-254, (float)-8.5, (float)119), transform.rotation * Quaternion.Euler (0f, 36.46f, 0f));
         }
     }
 
